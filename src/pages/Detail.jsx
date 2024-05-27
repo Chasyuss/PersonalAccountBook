78edc13749be521708fcb60abfdfd03a8d72d13b
chasyuss <cotn963@gmail.com>
2024-05-27 11:53:54 +0900

    [feat]: Detail page 데이터 가져오기

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { styled } from 'styled-components';
+import fakedata from './FakeData.json';
 
 const Detail = () => {
+    const { id } = useParams();
+    const allItems = JSON.parse(localStorage.getItem('allItems')) || [];
+    const item = [...fakedata, ...allItems].find(item => item.id === id);
+
+
     return (
-        <div>Detail</div>
+        <div>
+            <h2> 상세정보</h2>
+            <Date> {item.date} </Date>
+            <Item> {item.item} </Item>
+            <Amount> {item.amount} </Amount>
+            <Des> {item.description} </Des>
+        </div>
     )
 }
 
-export default Detail
+export default Detail;
+
+const Date = styled.div`
+    
+`;
+
+const Item = styled.div`
+    
+`;
+
+const Amount = styled.div`
+    
+`;
+
+const Des = styled.div`
+    
+`;
